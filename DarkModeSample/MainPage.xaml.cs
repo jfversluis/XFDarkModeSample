97af34dc33da6f6f8451ea3bb2f430260693013f
Gerald Versluis <gerald@verslu.is>
2020-09-07 08:43:10 +0200

    Initial commit

---
+++ DarkModeSample/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DarkModeSample
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Application.Current.RequestedThemeChanged += (_, args) =>
+            {
+                currentThemeLabel.Text = args.RequestedTheme.ToString();
+            };
+        }
+    }
+}
